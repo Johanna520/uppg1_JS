a490d9c75ec5d7d6ab85ada326e494d8a603d8bb
Johanna Ljungberg <johanna.ljungberg2@learnet.se>
2021-01-21 08:34:58 +0100

    start

---
+++ main.js
@@ -0,0 +1,15 @@
+/* TODO
+
+ * JSkod för sifferinputs fönstret (skapa blogginlägg) 
+
+
+
+ * JSkod per blogginlägg (toggla redigerbarhet)
+
+   https://developer.mozilla.org/en-US/docs/Web/API/HTMLElement/contentEditable
+
+   över 100 titlar ej ok
+   https://www.w3schools.com/js/js_errors.asp  
+
+   
+*/
