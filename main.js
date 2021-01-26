14f2bbf306ae6e0fe2cc4958e98f6cabebe2772a
Johanna Ljungberg <johanna.ljungberg2@learnet.se>
2021-01-26 15:39:35 +0100

    try som js

--- main.js
+++ main.js
@@ -17,4 +17,28 @@
    använd const och let inte var!s
 */
 //för att lättare upptäcka buggar skriver jag:
+//https://www.w3schools.com/js/js_strict.asp
+
 "use strict";
+  /* When the user clicks on the button, 
+          toggle between hiding and showing the dropdown content */
+          //se föreläsning 15.30!!!
+
+          
+          function myFunction() {
+            document.getElementById("myDropdown").classList.toggle("show");
+          }
+          
+          // Close the dropdown if the user clicks outside of it
+          window.onclick = function(event) {
+            if (!event.target.matches('.dropbtn')) {
+              var dropdowns = document.getElementsByClassName("dropdown-content");
+              var i;
+              for (i = 0; i < dropdowns.length; i++) {
+                var openDropdown = dropdowns[i];
+                if (openDropdown.classList.contains('show')) {
+                  openDropdown.classList.remove('show');
+                }
+              }
+            }
+          }
