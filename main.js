3f61e4490a3c88e70732508d9ec7c3c569d28d8b
Johanna Ljungberg <johanna.ljungberg2@learnet.se>
2021-01-27 15:58:49 +0100

    add button

--- main.js
+++ main.js
@@ -16,10 +16,18 @@
    
    använd const och let inte var!s
 */
-//för att lättare upptäcka buggar skriver jag:
-//https://www.w3schools.com/js/js_strict.asp
+
+//Jag använder "use script" för att enklare upptäcka buggar. Jag kan ex inte använda mig av odeklarerade variabler vid användandet av "use strict". "
 
 "use strict";
+
+
+/*här har jag skrvit kod för min dropdown-button. 
+Jag skriver const för att  . . . 
+Jag hämtar första objektet med id "btn" samt id "arkiv".
+
+*/
+
 {
 
 const button = document.getElementById("btn");
@@ -29,62 +37,25 @@ const arkiv = document.getElementById("arkiv");
 arkiv.style.display = "none";
 
 button.addEventListener("click", function () {
-  if (arkiv.style.display == "none") {
+  if (arkiv.style.display === "none") {
     arkiv.style.display = "block";
-  } else {
+  } 
+  
+  else {
     arkiv.style.display = "none";
   }
 });
 }
 
-
-
 {
-  
-    let inputBox = document.querySelector("#input-number");
-  
-    inputBox.addEventListener("input", (event) => {
-      removeAllSections();
-      makeSections(parseInt(event.currentTarget.value));
-    });
-  
-  
-  let makeSections = (count) => {
-    for (var i = 0; i < count; i++) {
-      var parent = document.querySelector("main");
-  
-      var child = document.createElement("article");
-      var title = document.createElement("h1");
-      var blurb = document.createElement("p");
-  
-      title.innerText = "Title " + i;
-      blurb.innerText = `Skriv ditt blogginlägg.`;
-  
-      makeEditable(title);
-      makeEditable(blurb);
-  
-      child.append(title);
-      child.append(blurb);
-      parent.append(child);
-    }
-  };
-  
-  let removeAllSections = () => {
-    var test_sections = document.querySelectorAll("article");
-    for (var i = 0; i < test_sections.length; i++) {
-      test_sections[i].remove();
-    }
-  };
-  
-  let makeEditable = (elem) => {
-    elem.addEventListener("click", (event) => {
-      elem.contentEditable = true;
-      elem.focus();
-    });
-    elem.addEventListener("blur", (event) => {
-      elem.contentEditable = false;
-    });
-  };
-  
+const addButton = document.getElementById("add");
+
+
+addButton.addEventListener("click", function (){
+    const x = document.createElement("TEXTAREA");
+  const t = document.createTextNode("Write something here....");
+  x.appendChild(t);
+  document.body.appendChild(x);
+});
 
 }
