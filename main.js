b9d546880c77c19d5a0a6031221d42d7a7e9485d
Johanna Ljungberg <johanna.ljungberg2@learnet.se>
2021-01-28 14:45:10 +0100

    kommentarer

--- main.js
+++ main.js
@@ -1,32 +1,85 @@
-/* TODO
+/* 
+Vad jag gjort för att felsöka/undvika buggar:
 
- * JSkod för sifferinputs fönstret (skapa blogginlägg) 
-
-
-
- * JSkod per blogginlägg (toggla redigerbarhet)
-
-   https://developer.mozilla.org/en-US/docs/Web/API/HTMLElement/contentEditable
-
-   över 100 titlar ej ok
-   https://www.w3schools.com/js/js_errors.asp  
+https://www.w3schools.com/js/js_strict.asp
+- Jag använder "use script" för att skriva säkrare JavaScirpt-kod och en mer "ren" kod.
+  Jag kan ex inte använda mig av odeklarerade variabler vid användandet av "use strict", det isåfall kommer att kastas ett fel,
+  däför är samtliga variabler i denna kod deklarerade med const.
 
 
-   gör så det ej går att ta minus på post. = felmeddelande
-   
-   använd const och let inte var!s
+  https://www.w3schools.com/js/js_conventions.asp
+  - jag använder mig av de generella riktlinjer som finns på w3school över hur Javascript koden ska  se ut. 
+    Ex:
+    jag använder mig av camelcase för att namnge mina variabler och funktioner. 
+    skapar mellanrum vid operatorer (i detta fall = ) samt efter kommatecken. 
 
-   f12 för consolen för att förstå varför min JavaScritp inte fungerade. . 
+  https://www.w3schools.com/js/js_debugging.asp
+  - jag har aktiverat debbugging genom F12 och via consollen försökt förstå varför och vart i min kod fel uppstår.
+
+https://www.w3schools.com/js/js_best_practices.asp
+  - i min dropdown (som jag kommenterat ut) använder jag === istället för ==
+  - jag undviker new
 */
 
-/*Jag använder "use script" för att enklare upptäcka buggar. 
-Jag kan ex inte använda mig av odeklarerade variabler vid användandet av "use strict". */
+
 
 "use strict";
 
 
-/*Dropdown-button. */
 
+/*I min kod för <button id="add"> och <button id="delete"> 
+förändrar jag och uppdaterar DOM genom att skapa och ta bort ett element. */
+
+{
+
+  /*Add-button
+
+*/
+  const addButton = document.getElementById("add"); //jag hämtar min <button id="add">- element för att kunna förändra elementet
+
+  addButton.addEventListener("click", function addTextBox (){ //genom addEdventListener kan jag tilldela eventet "click" tillsammans
+                                                              //med functionen. I detta fall vad som sker när användaren trycker på <button>.
+
+  const textArea = document.createElement("TEXTAREA");  //Jag skapar ett element (TEXTAREA) genom creatELement. 
+  textArea.setAttribute("id", "textbox")  //Jag ger elementet ett attribut (id), settAttribute(id ="textbox").
+
+
+  const text = document.createTextNode("Write something here...."); //För att lägga text i min textarea gör jag en textNode ..
+  textArea.appendChild(text); //.. och lägger till texten i textarea.
+  document.body.appendChild(textArea); //Därefter lägger jag till mitt nya element i <body>.
+});
+}
+
+
+
+//Delete-button
+{
+  const deleteButton = document.getElementById("delete");//jag hämtar min <button id="delete">- element för att kunna förändra elementet
+
+  deleteButton.addEventListener("click", function deleteTextBox (){//genom addEdventListener kan jag tilldela eventet "click" tillsammans
+                                                                  //med functionen. I detta fall vad som sker när användaren trycker på <button>.
+
+    
+  const textArea = document.getElementById("textbox");  //För att hitta elelementet jag vill ta bort använder jag getELementbyId. 
+                                                //då jag gav elementet Textarea ett nytt Id(genon addAttribute) skriver jag textbox mellan (). 
+  textArea.remove();                            // därefter tilldelar jag remove()-metoden till elementet. 
+});
+}
+
+
+
+{
+  //VARFÖR CONSOLE?
+  console.log("Hej på dig");
+}
+
+
+
+
+/*Dropdown-button. 
+OBS! vet inte riktigt vad jag ska använda den till.
+Men testade lite, ev kan den, när den är klar, användas till
+att klicka in på olika kategotier/inlägg.
 {
 
 const button = document.getElementById("btn"); // 
@@ -44,40 +97,5 @@ button.addEventListener("click", function () {
     arkiv.style.display = "none";
   }
 });
-}
+}*/
 
-
-/*Add-button
-
-*/
-
-{
-
-
-
-const addButton = document.getElementById("add");
-
-
-addButton.addEventListener("click", function addTextBox (){
-    const x = document.createElement("TEXTAREA");   //    Jag skapar ett element, <textarea>.
-    x.setAttribute("id", "textbox") // Ger elementet ett ID. 
-
-
-  const t = document.createTextNode("Write something here...."); // Jag skapar text i textarean.
-  x.appendChild(t); // 
-  document.body.appendChild(x); //Lägger till  textarea till <body> 
-});
-
-
-}
-
-//Delete-button
-{
-const deleteButton = document.getElementById("delete");
-
-deleteButton.addEventListener("click", function deleteTextBox (){
-    const x = document.getElementById("textbox");  
-    x.remove();
-
-} );
-}
